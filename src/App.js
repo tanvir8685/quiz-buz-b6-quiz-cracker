a2b614f066aa0a11a9db57a03036e76bd6dea040
tanvir hridoy <tanvirhridoy1011@gmail.com>
2022-10-12 04:45:11 +0600

    basic router set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 
 import { Button } from 'react-bootstrap';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Quiz from './components/Quiz/Quiz';
+
+import Main from './layouts/Main';
 
 function App() {
+  const router=createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main>,
+      
+      children:[
+        {
+          path:'/',
+          element:<Home></Home>
+        },
+        {
+          path:'/quiz',
+          element:<Quiz></Quiz>
+        }
+      ]
+
+    }
+  ])
   return (
     <div className="App">
       <Button variant="danger">Check</Button>
+      <RouterProvider router={router} ></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Quiz/Quiz.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Quiz = () => {
+    return (
+        <div>
+            <h2>This is Quiz</h2>
+        </div>
+    );
+};
+
+export default Quiz;
