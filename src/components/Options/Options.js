d6a20aefcd5bfc4f3a7e1fde11ba470076139b9c
tanvir hridoy <tanvirhridoy1011@gmail.com>
2022-10-12 18:10:19 +0600

    Correct ans with btn

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Options = ({options}) => {
-    console.log(options)
+const Options = ({options,qustion}) => {
+    // console.log(qustion)
     const clicked=(id)=>{
+        alert('you are choice',id)
         console.log('this is clicked',id)
     }
-    console.log(options)
+    // console.log(options)
     return (
         <div>
             <div onClick={()=>clicked(options)} className='m-5'>
--- src/components/QuizQuestion/QuizQuestion.js
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import Options from '../Options/Options';
 
 const QuizQuestion = ({qustion}) => {
+    console.log(qustion.correctAnswer)
+    const[correct,setCorrect]=useState();
+    const correctbtn=(id)=>{
+        console.log('clicked',id)
+        setCorrect(qustion.correctAnswer)
+    }
     
     const{question}=qustion;
     const qustionOptions=qustion.options;
     // console.log(qustionOptions)
     return (
         <div>
-            <h4>{question}</h4>
+            <h4>{question}</h4> <Button onClick={()=>correctbtn(qustion.id)}>See</Button>
             {
-                qustionOptions.map(options=><Options options={options}></Options>)
+                qustionOptions.map((options,id)=><Options key={id} qustion={qustion} options={options}></Options>)
             }
 
-            
+            <h3>Correct Answer:{correct}</h3>
         </div>
     );
 };
