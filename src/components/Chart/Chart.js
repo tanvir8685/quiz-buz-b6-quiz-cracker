85f93b5093dbda04c2cd3edeb2412a3891c4bff9
tanvir hridoy <tanvirhridoy1011@gmail.com>
2022-10-12 22:22:16 +0600

    adding readme file

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Tooltip } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 import { Line, LineChart,  XAxis, YAxis } from 'recharts';
 
@@ -14,6 +15,7 @@ const Chart = () => {
             <Line type="monotone" dataKey="total" stroke="#82ca9d" />
             <XAxis dataKey="name" />
           <YAxis />
+          <Tooltip />
             </LineChart>
         </div>
     );
