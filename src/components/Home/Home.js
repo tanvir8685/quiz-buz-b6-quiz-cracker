d53f3699d02cff959a4b5f123eee2ccbcc5cb350
tanvir hridoy <tanvirhridoy1011@gmail.com>
2022-10-12 07:12:33 +0600

    header pic part

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home pic </h1>
-            <h2>And this is other things</h2>
+            
+            <div>
+            <Card className="bg-dark text-white h-50">
+      <Card.Img  src="https://image.shutterstock.com/image-vector/neon-quiz-sign-vector-design-600w-1722883588.jpg"  className='' alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Title className='h-25 text-center fw-bold fs-1 mt-10 text-info' >Welcome to your Quiz</Card.Title>
+        <Card.Text className='h-25 text-center fw-bold fs-1 mt-10 '>
+          Choice your Subject wisely you have to very serious in this 
+        </Card.Text>
+        <Card.Text  className='h-25 text-center fw-bold fs-1 mt-10'>Good Luck</Card.Text>
+      </Card.ImgOverlay>
+    </Card>
+           
+        </div>
+
+            
+
+            <div className='mt-5'>
+                <h1>Our main content</h1>
+            </div>
+
+
         </div>
     );
 };
